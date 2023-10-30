c24ed738b553c813e4bf3f07a59f83ff957030d0
kaunfused <royakashdeep654@gmail.com>
2023-10-31 00:43:38 +0530

    changes

--- src/components/Trains.jsx
+++ src/components/Trains.jsx
@@ -8,43 +8,40 @@ export const loader = async () => {
 
 const Trains = () => {
   const { data } = useLoaderData();
+
   const res = data.map((train) => {
-    return <SingleTrain {...train} />;
+    return (
+      <div>
+        <h2>Train: {train.Train}</h2>
+        <h1>
+          Train Name: {train.TrainName} Train Number: {train.trainNo}
+        </h1>
+        <p>Start: {train.Source}</p>
+        <p>Destination: {train.Destination}</p>
+        <h3>
+          Arrival: {train.ArrivalTime} Departure: {train.DepartureTime}
+        </h3>
+
+        <h1>Seats: </h1>
+        {train["3AC_Num"] === null ? <></> : <h2>3AC: {train["3AC_Num"]}</h2>}
+        {train["3E_Num"] === null ? <></> : <h2>3E: {train["3E_Num"]}</h2>}
+        {train["2AC_Num"] === null ? <></> : <h2>2AC: {train["2AC_Num"]}</h2>}
+        {train["1AC_Num"] === null ? <></> : <h2>1AC: {train["1AC_Num"]}</h2>}
+        {train["AC_CharCar_Num"] === null ? (
+          <></>
+        ) : (
+          <h2>AC chair car: {train["AC_CharCar_Num"]}</h2>
+        )}
+        {train["ExechairCar_Num"] === null ? (
+          <></>
+        ) : (
+          <h2>Executive Chair car: {train["ExechairCar_Num"]}</h2>
+        )}
+      </div>
+    );
   });
 
   return { res };
-};
-
-const SingleTrain = ({ train }) => {
-  return (
-    <div>
-      <h2>Train: {train.Train}</h2>
-      <h1>
-        Train Name: {train.TrainName} Train Number: {train.trainNo}
-      </h1>
-      <p>Start: {train.Source}</p>
-      <p>Destination: {train.Destination}</p>
-      <h3>
-        Arrival: {train.ArrivalTime} Departure: {train.DepartureTime}
-      </h3>
-
-      <h1>Seats: </h1>
-      {train["3AC_Num"] === null ? <></> : <h2>3AC: {train["3AC_Num"]}</h2>}
-      {train["3E_Num"] === null ? <></> : <h2>3E: {train["3E_Num"]}</h2>}
-      {train["2AC_Num"] === null ? <></> : <h2>2AC: {train["2AC_Num"]}</h2>}
-      {train["1AC_Num"] === null ? <></> : <h2>1AC: {train["1AC_Num"]}</h2>}
-      {train["AC_CharCar_Num"] === null ? (
-        <></>
-      ) : (
-        <h2>AC chair car: {train["AC_CharCar_Num"]}</h2>
-      )}
-      {train["ExechairCar_Num"] === null ? (
-        <></>
-      ) : (
-        <h2>Executive Chair car: {train["ExechairCar_Num"]}</h2>
-      )}
-    </div>
-  );
 };
 
 export default Trains;
