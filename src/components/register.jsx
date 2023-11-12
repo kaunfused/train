7045be3b47d617ccce8092ee2b408947fafd6ba5
kaunfused <royakashdeep654@gmail.com>
2023-11-12 23:05:28 +0530

    horizontal scroll removed?

--- src/components/register.jsx
+++ src/components/register.jsx
@@ -122,7 +122,7 @@ function Register() {
 
 const Container = styled.div`
   height: 100vh;
-  width: 100vw;
+  /* width: 100vw; */
   display: flex;
   flex-direction: column;
   justify-content: center;
