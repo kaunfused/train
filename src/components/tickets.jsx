f5d780087914b7c9fd311712ea4f8e6d70f5f0e8
kaunfused <royakashdeep654@gmail.com>
2023-11-16 02:10:43 +0530

    changes made to tickets part 2

--- src/components/tickets.jsx
+++ src/components/tickets.jsx
@@ -6,7 +6,7 @@ let today = new Date();
 let dd = String(today.getDate()).padStart(2, "0");
 let mm = String(today.getMonth() + 1).padStart(2, "0");
 let yyyy = today.getFullYear();
-today = mm + "/" + dd + "/" + yyyy;
+today = dd + "/" + mm + "/" + yyyy;
 
 function Ticket() {
   let data = useLoaderData();
