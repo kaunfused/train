48b323849c67bd48aa1227938d6de75f200eafb3
kaunfused <royakashdeep654@gmail.com>
2023-11-15 20:46:47 +0530

    changes in tickets?

--- src/components/tickets.jsx
+++ src/components/tickets.jsx
@@ -6,6 +6,13 @@ function Ticket() {
     <Wrapper>
       <h3>Your seat has been booked!!</h3>
       <h1>Passenger details:</h1>
+
+      <div className="destination">
+        <h2>From </h2>
+        <p>Howrah</p>
+        <h2>To </h2>
+        <p>Puri</p>
+      </div>
       <table>
         <tr>
           <th>Name</th>
@@ -36,6 +43,22 @@ const Wrapper = styled.section`
   td {
     text-align: center;
     padding: 0 1.5rem;
+  }
+
+  .destination {
+    /* border: 2px solid white; */
+    display: flex;
+    justify-content: space-evenly;
+    align-items: center;
+    min-width: 25rem;
+    h2 {
+      font-size: 2rem;
+      color: red;
+    }
+    p {
+      font-size: 1.5rem;
+      font-weight: 700;
+    }
   }
 `;
 
